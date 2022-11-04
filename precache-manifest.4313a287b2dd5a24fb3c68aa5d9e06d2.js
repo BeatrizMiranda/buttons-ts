67dee03a2511fe0d289563be31bb9c50822339e9
Beatriz Miranda <beatriz.miranda@thoughtworks.com>
2022-11-04 09:26:37 -0300

    Updates

---
+++ precache-manifest.4313a287b2dd5a24fb3c68aa5d9e06d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edd003a5271bfc60991e",
+    "url": "/buttons-ts/static/css/main.b6d451b5.chunk.css"
+  },
+  {
+    "revision": "edd003a5271bfc60991e",
+    "url": "/buttons-ts/static/js/main.edd003a5.chunk.js"
+  },
+  {
+    "revision": "80c6d3076e5889442bd7",
+    "url": "/buttons-ts/static/js/1.80c6d307.chunk.js"
+  },
+  {
+    "revision": "66a0f2ac178caaaa4b43",
+    "url": "/buttons-ts/static/js/runtime~main.66a0f2ac.js"
+  },
+  {
+    "revision": "02a046c129d9d30ac11f486639bf3ef6",
+    "url": "/buttons-ts/static/media/Preloader.02a046c1.svg"
+  },
+  {
+    "revision": "f0be5ef04bf13c684cda0624bbf46d83",
+    "url": "/buttons-ts/index.html"
+  }
+];
